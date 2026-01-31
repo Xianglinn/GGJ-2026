74b3f47193f7ddded12a3e2e3163ced1a0a8fff6
Xianglinn <simonwithnoe.wang@mail.utoronto.ca>
2026-02-01 00:01:06 +0800

    创建所有Prefab，并不包含物品栏制作

---
+++ Assets/scripts/ResourceMovement/TrashBinDrop.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// 垃圾桶：拖拽物品放入后销毁，显示 BinOn
+public class TrashBinDrop : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject binOn;
+    [SerializeField] private GameObject binOff;
+
+    private void Awake(){
+        SetBinState(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        SetBinState(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        SetBinState(false);
+    }
+
+    public void OnDrop(PointerEventData eventData){
+        SetBinState(true);
+
+        if(eventData.pointerDrag == null)
+        {
+            SetBinState(false);
+            return;
+        }
+
+        DragByInterface dragItem = eventData.pointerDrag.GetComponent<DragByInterface>();
+        if(dragItem != null)
+        {
+            Destroy(dragItem.gameObject);
+        }
+
+        SetBinState(false);
+    }
+
+    private void SetBinState(bool isOn){
+        if(binOn != null)
+        {
+            binOn.SetActive(isOn);
+        }
+        if(binOff != null)
+        {
+            binOff.SetActive(!isOn);
+        }
+    }
+}
